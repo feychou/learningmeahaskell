cef461694de7e5e2ea82c5368f27ba813e449bb5
Fey <fey@Feyputer.(none)>
2014-04-19 20:06:08 +0200

    folds and scans

--- ex/chap5.hs
+++ ex/chap5.hs
@@ -117,3 +117,10 @@ head' = foldr1 (\x _ -> x)
 
 last' :: [a] -> a
 last' = foldl1 (\_ x -> x)
+
+{-| 
+  How many elements does it take for the sum of 
+  the roots of all natural numbers to exceed 1000?
+-}
+sqrtSums :: Int
+sqrtSums = length (takeWhile (<1000) (scanl1 (+) (map sqrt [1..]))) + 1
